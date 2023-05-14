5767fc904ed375ae6bd3074b30131fe79301c0ae
thahira97 <thahirasheerin3@gmail.com>
2023-05-14 18:36:11 -0400

    Add validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+    validates :first_name, presence: { message: "Name can't be blank" }
+    validates :last_name, presence: { message: "Name can't be blank" }
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+  
 end
