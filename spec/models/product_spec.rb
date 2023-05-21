d6cf32715ed316f5efc05ff7a3461fc7743bd8f5
thahira97 <thahirasheerin3@gmail.com>
2023-05-21 14:40:29 -0400

    Add test to check if the price is true

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Product, type: :model do
       @product = @category.products.new(name: "Green tea")
       expect(@product.name).to eq("Green tea")
     end
+    it 'should have a price' do
+      @category = Category.new
+      @product = @category.products.new(price: Money.new(120))
+      expect(@product.price).to eq(Money.new(120))
+    end
   end
 end
