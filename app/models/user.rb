986ddd9b68d3960aa98498b7602ec747ac76a282
thahira97 <thahirasheerin3@gmail.com>
2023-05-21 13:32:10 -0400

    Add rspec to the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
-  
-    validates :first_name, presence: { message: "Name can't be blank" }
-    validates :last_name, presence: { message: "Name can't be blank" }
+
+    validate :validate_names
+    validate :validate_names
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
-  
+    
+    def validate_names 
+      if !first_name && !last_name
+        errors.add(:first_name, "Names can't be blank")
+        errors.add(:last_name, "Names can't be blank")
+      end
+    end
 end
